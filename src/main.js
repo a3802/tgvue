fa20b0b958e0812b97f9d893c75dd18bbf55b4db
a3802 <253092329@qq.com>
2023-05-16 05:21:02 +0800

    test33

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
+import { CountDown } from 'vant';
 import App from './App.vue'
 // import 'amfe-flexible';
 import './libs/rem.js';
 import router from './router/index';
 const app = createApp(App)
-app.use(router)
+app.use(router).use(CountDown)
 app.mount('#app')
