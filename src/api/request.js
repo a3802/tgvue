c1ea9edc337aa61ca182f32d622f25f1c91bc10a
a3802 <253092329@qq.com>
2023-07-26 17:01:20 +0800

    diisg

--- src/api/request.js
+++ src/api/request.js
@@ -2,13 +2,14 @@ import axios from 'axios'
 
 // 创建一个 axios 实例
 const service = axios.create({
-    baseURL: 'http://tgqy.yueyueyouqian.cn/api', // 所有的请求地址前缀部分
+    baseURL: '/api', // 所有的请求地址前缀部分
     timeout: 60000, // 请求超时时间毫秒
     withCredentials: true, // 异步请求携带cookie
     headers: {
         // 设置后端需要的传参类型
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json',
     },
 })
 
--- vue.config.js
+++ vue.config.js
@@ -41,11 +41,11 @@ module.exports = {
     devServer: {
         port: 8080, // 端口
         proxy: {
-            '/localhost': { //   若请求的前缀不是这个'/api'，那请求就不会走代理服务器
-                target: 'http://localhost:8078', //这里写路径 
-                pathRewrite: { '^/localhost': '' }, //将所有含/api路径的，去掉/api转发给服务器
-                ws: true, //用于支持websocket
-                changeOrigin: true //用于控制请求头中的host值
+            '/api': { //   若请求的前缀不是这个'/api'，那请求就不会走代理服务器
+                target: 'http://tgqy.yueyueyouqian.cn', //这里写路径 
+                pathRewrite: { '^/api': '' }, //将所有含/api路径的，去掉/api转发给服务器
+                changeOrigin: true, //用于控制请求头中的host值,
+                ws: true, //代理websocked
             },
         }
     }
